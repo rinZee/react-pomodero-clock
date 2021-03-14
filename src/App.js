a7c536cbc8a2e3b5651dc0c420859b54f6898533
unknown <trex33160@gmail.com>
2021-03-14 14:10:08 +0545

    changes added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,30 @@ function App() {
   return (
     <div className="App">
       
+<div id='break-label'>Break Length</div>
+<div id="session-label">Session Length</div>
+
+<button id="break-decrement"></button>
+<button id="session-decrement"></button>
+
+<button id="break-increment"></button>
+<button id="session-increment"></button>
+
+<div id="break-length">5</div>
+<div id="session-length">25</div>
+
+
+
+
+
+
+
+<div id="timer-label">Session</div>
+
+<div id="time-left">25:00</div>
+<div id="start_stop"></div>
+<div id="reset"></div>
+
     </div>
   );
 }
